af35780f30f5de1d0c19bb889534f1bfd76a2e4d
Max <mcapener@gmail.com>
2014-11-12 15:48:43 -0700

    Do not re-check number multiple times.

--- problem_4_euler_in_python.py
+++ problem_4_euler_in_python.py
@@ -19,12 +19,12 @@ def find_palindromes():
 	'''Returns a list of all palindromes found for the product of two 3-digit
 	numbers.'''
 	for i in range(100, 1000):
-		for j in range(100, 1000):
+		for j in range(i, 1000):
 			product = i * j
 			if is_palindrome(product):
 				yield product
 
-# This script takes ~1.5 secs to complete.
+# This script takes ~0.8 secs to complete.
 # The challenge is... if you start from 999 down and break when you find the
 # first value, how do you know that this is the max product?
 if __name__ == '__main__':
