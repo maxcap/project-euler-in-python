017632fc5255e580f391b02254a64a16d51eb0f5
Max <mcapener@gmail.com>
2014-11-12 01:21:06 -0700

    Square root max limit improvement.

--- euler-in-python-problem3.py
+++ euler-in-python-problem3.py
@@ -15,19 +15,19 @@ What is the largest prime factor of the number N?
 
 # From Project Euler PDF: After first improvement
 def all_prime_factors(n):
-	yield 1
 	# 2 is the only even prime, so by treating it separately we can increase
 	# factor with 2 every step.
 	while n % 2 == 0:
 		n /= 2
-		yield 2
+	# Start while loop with factor 3
 	factor = 3
-	while n > 1:
+	while n > 1 and factor ** 2 <= n:
 		while n % factor == 0:
-			yield factor
 			n /= factor
+			yield factor
 		factor += 2  # loop over odd only. We dealt with all evens earlier.
+	yield n
 
 if __name__ == '__main__':
-	N = 600851475144
+	N = 600851475143
 	print max(all_prime_factors(N))
